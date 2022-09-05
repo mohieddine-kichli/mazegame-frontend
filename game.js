0300e12909a575a75beb27a7a1320bcadaaf1a9f
mohieddine-kichli <mohieddine.kichli@gmail.com>
2022-09-05 08:44:26 +0300

    frontend loading html

---
+++ game.js
@@ -0,0 +1,22 @@
+// Loading HTML page fully
+
+window.addEventListener('load', (event) => {
+
+    console.log('The page has fully loaded');
+
+});
+
+// Start Game Script
+
+const start_button = document.getElementById("start");  
+
+start_button.addEventListener("click", startgame); // begin game on click of green start button
+
+function startgame () {
+
+    // while (test) {
+
+
+    // }
+
+}
